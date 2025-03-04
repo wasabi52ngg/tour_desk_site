109a0306c457eadd621ccebf45361da1dfb84699
wasabi <vovasharafutdinov@mail.ru>
2025-03-04 12:02:14 +0300

    added site map

--- sitetour/sitemaps.py
+++ sitetour/sitemaps.py
@@ -1,11 +1,14 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Tour
+from django.utils import timezone
+
+
 class PostSitemap(Sitemap):
-	changefreq = 'monthly'
-	priority = 0.9
+    changefreq = 'monthly'
+    priority = 0.9
 
-	def items(self):
-		return Tour.objects.all()
+    def items(self):
+        return Tour.objects.all()
 
-	def lastmod(self,obj):
-		return obj.time_update()
+    def lastmod(self, obj):
+        return timezone.now()
